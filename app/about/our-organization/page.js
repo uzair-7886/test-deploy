ba65b79d00ef08f8e7dd26ead669dfa4e92272c4
Uzair Khan <jerry.khan7886@gmail.com>
2025-01-29 19:05:34 +0500

    Add HeroSectionShared component and update organization page layout

--- app/about/our-organization/page.js
+++ app/about/our-organization/page.js
@@ -3,11 +3,13 @@ import About from "@/app/components/about/our-organization/About"
 import WhyOxford from "@/app/components/about/our-organization/WhyOxford"
 import UniOxford from "@/app/components/about/our-organization/UniOxford"
 import CityOxford from "@/app/components/about/our-organization/CityOxford"
+// import HeroSectionShared from "@/app/components/shared/HeroSectionShared"
 
 const page = () => {
     return (
       <>
         <HeroSectionOrganization />
+        {/* <HeroSectionShared textWhite="OUR" textYellow="ORGANIZATION" imagePath="/Our Organization Page/hero.png"/> */}
         <About />
         <WhyOxford />
         <UniOxford />
